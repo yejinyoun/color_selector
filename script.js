65de0eb7bf9f2ab3ba17518c858e99e4c5bc0703
yejinyoun <yejinyoun@icloud.com>
2023-02-15 15:00:10 +0100

    js function structure set up

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const picker = document.querySelector("input");
+
+picker.addEventListener("input", init);
+
+function init() {
+  console.log("start!");
+  currentColor();
+  displayColor();
+  displayColorCode();
+}
+
+function currentColor() {
+  function readHEX() {}
+  function HEXtoRGB() {}
+  function RGBtoHEX() {}
+}
+
+function displayColor() {}
+
+function displayColorCode() {}
