2c090ea9eaf22b5031e28897852811a53b9bdd18
yejinyoun <yejinyoun@icloud.com>
2023-02-15 21:06:39 +0100

    improved js basic structure of functions

--- improved.js
+++ improved.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("hihi");
+
+window.addEventListener("DOMContentLoaded", init);
+//init function executes when DOMContentLoaded
+
+function init() {
+  document.querySelector("input").addEventListener("input", showColor);
+}
+
+function showColor() {
+  function showColorBox(csscode) {}
+  function showColorCode(colorcode) {
+    function showHEX(hexcode) {}
+    function showRGB(rgbcode) {}
+    function showHSL(hslcode) {}
+  }
+  function getColorCode() {
+    function getHEX(selectedColor) {}
+    function hexToRGB(hexcode) {}
+    function rgbToHSL(rgbcode) {}
+    function rgbToCSS(rgbcode) {}
+  }
+}
