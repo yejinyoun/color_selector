55feb2d2ace7a92bc5f5ded53e9e81102a0f88da
yejinyoun <yejinyoun@icloud.com>
2023-02-15 15:16:32 +0100

    js setup w init

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 "use strict";
 
-const picker = document.querySelector("input");
+window.addEventListener("DOMContentLoaded", init);
+//init function executes when DOMContentLoaded
 
-picker.addEventListener("input", init);
+let picker;
+let pickedColor;
 
 function init() {
-  console.log("start!");
+  //detects when the color is selected and executes all the function
+  picker = document.querySelector("input");
+
+  picker.addEventListener("input", currentColor);
+
   currentColor();
   displayColor();
   displayColorCode();
 }
 
 function currentColor() {
+  pickedColor = picker.value;
+  console.log(pickedColor);
   function readHEX() {}
   function HEXtoRGB() {}
   function RGBtoHEX() {}
